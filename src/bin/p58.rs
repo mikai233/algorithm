8b03a1479238fcc6b5c5e10c3ab4f776583a2ea1
mikai <dreamfever2017@yahoo.com>
2025-08-06 22:44:56 +0800

    58. Length of Last Word

---
+++ src/bin/p58.rs
@@ -0,0 +1,23 @@
+struct Solution;
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let mut len = 0;
+        for &b in s.as_bytes().iter().rev() {
+            if b == b' ' {
+                if len > 0 {
+                    break;
+                }
+            } else {
+                len += 1;
+            }
+        }
+        len
+    }
+}
+
+fn main() {
+    println!("{}", Solution::length_of_last_word("Hello World".to_string()));
+    println!("{}", Solution::length_of_last_word("   fly me   to   the moon  ".to_string()));
+    println!("{}", Solution::length_of_last_word("luffy is still joyboy".to_string()));
+}
