6c5d23117d83090011a3e7b4b56bdd201057bcf0
mikai <dreamfever2017@yahoo.com>
2025-08-15 22:05:43 +0800

    35. Search Insert Position

---
+++ src/bin/p35_non_recursion.rs
@@ -0,0 +1,24 @@
+struct Solution;
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        if nums.is_empty() { return 0; }
+        let (mut left, mut right) = (0, nums.len());
+        while left < right {
+            let mid = left + (right - left) / 2;
+            let mid_val = nums[mid];
+            if mid_val < target {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        left as i32
+    }
+}
+
+fn main() {
+    println!("{}", Solution::search_insert(vec![1, 3, 5, 6], 5));
+    println!("{}", Solution::search_insert(vec![1, 3, 5, 6], 2));
+    println!("{}", Solution::search_insert(vec![1, 3, 5, 6], 7));
+}
