e5916c73a77fd30e4dd4a3d13e49ffd0d79ca41c
mikai <dreamfever2017@yahoo.com>
2026-03-04 14:59:27 +0800

    q329

--- src/algorithms/q329_longest_increasing_path_in_a_matrix.rs
+++ src/algorithms/q329_longest_increasing_path_in_a_matrix.rs
@@ -1,18 +1,11 @@
-struct Solution;
+use crate::common::solution::Solution;
 
 impl Solution {
     pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
         let m = matrix.len();
-        if m == 0 {
-            return 0;
-        }
         let n = matrix[0].len();
-        if n == 0 {
-            return 0;
-        }
-
         let mut dp = vec![vec![0; n]; m];
-        let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+        let dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 
         fn dfs(
             i: usize,
@@ -21,22 +14,22 @@ impl Solution {
             dp: &mut Vec<Vec<i32>>,
             dirs: &[(i32, i32)],
         ) -> i32 {
-            if dp[i][j] != 0 {
-                return dp[i][j];
+            let record = dp[i][j];
+            if record != 0 {
+                return record;
             }
-
             let m = matrix.len() as i32;
             let n = matrix[0].len() as i32;
-            let val = matrix[i][j];
             let mut best = 1;
-            for (dx, dy) in dirs {
+            let val = matrix[i][j];
+            for &(dx, dy) in dirs {
                 let ni = i as i32 + dx;
                 let nj = j as i32 + dy;
                 if ni >= 0 && ni < m && nj >= 0 && nj < n {
                     let ui = ni as usize;
                     let uj = nj as usize;
                     if matrix[ui][uj] > val {
-                        best = best.max(1 + dfs(ui, uj, matrix, dp, dirs));
+                        best = best.max(1 + dfs(ui, uj, matrix, dp, dirs))
                     }
                 }
             }
@@ -45,19 +38,11 @@ impl Solution {
         }
 
         let mut ans = 0;
-
         for i in 0..m {
             for j in 0..n {
-                ans = ans.max(dfs(i, j, &matrix, &mut dp, &dirs))
+                ans = ans.max(dfs(i, j, &matrix, &mut dp, &dirs));
             }
         }
-
         ans
     }
-}
-
-fn main() {
-    let matrix = vec![vec![9, 9, 4], vec![6, 6, 8], vec![2, 1, 1]];
-    let n = Solution::longest_increasing_path(matrix);
-    println!("{n}");
 }
